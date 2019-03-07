2e2b4d0874a7a2c0edd4e070b88779d374617130
denisijcu <denisijcu266@gmail.com>
2019-03-07 13:38:31 -0500

    first commit

---
+++ precache-manifest.750b7c7c5cd82e467253a958346357eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "62d1b8e242378f381f531a5616835576",
+    "url": "/static/media/storm.62d1b8e2.png"
+  },
+  {
+    "revision": "2fb337dbffc191d2f4bf14c458e1e73c",
+    "url": "/static/media/storm.2fb337db.png"
+  },
+  {
+    "revision": "d447e8125304ac042eb800ba9db70100",
+    "url": "/static/media/shower-rain.d447e812.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3e3bcfdbe5544ff331f8",
+    "url": "/static/js/main.3e3bcfdb.chunk.js"
+  },
+  {
+    "revision": "a6d505210b7a6e89e458",
+    "url": "/static/js/2.a6d50521.chunk.js"
+  },
+  {
+    "revision": "3e3bcfdbe5544ff331f8",
+    "url": "/static/css/main.1c286302.chunk.css"
+  },
+  {
+    "revision": "3e8dd646be102060abaa175a137009b5",
+    "url": "/index.html"
+  }
+];
